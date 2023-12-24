98a7f2062be82a6bc0dcc9c8a45a98e58a81404b
AcidTCell <cimafrancajasper.personal@gmail.com>
2023-12-24 18:35:11 +0800

    Add input functionality for grid size

--- script.js
+++ script.js
@@ -1,40 +1,53 @@
 let container = document.querySelector('#container');
 let newDiv = document.createElement('div');
-
-let column = 32;
-let row = 32;
+let gridButton = document.querySelector('#gridButton');
 
 
+gridButton.addEventListener('click', getGridSize);
 
-for (let i = 1; i<=row; i++){
-    let row = document.createElement('div');
-    row.classList.add('row');
-    container.appendChild(row);
-    for (let j = 1; j<=column; j++){
-        let box = document.createElement('div');
-        box.classList.add('box');
-        
-        row.appendChild(box);
-        box.addEventListener('mousedown', function() {
-            draw(box);
-        });
+function getGridSize(){
+    clearGrid();
+    let gridSize = prompt('What dimensions are you seeking? ');
+    let column = gridSize;
+    let row = gridSize;
+    createGrid(column, row);
+}
 
-        // Add mouseover event listener to continue drawing while dragging
-        box.addEventListener('mouseover', function() {
-            if (isDrawing) {
+function createGrid(column, row){
+    for (let i = 1; i<=row; i++){
+        let row = document.createElement('div');
+        row.classList.add('row');
+        container.appendChild(row);
+        for (let j = 1; j<=column; j++){
+            let box = document.createElement('div');
+            box.classList.add('box');
+            
+            row.appendChild(box);
+            box.addEventListener('mousedown', function() {
                 draw(box);
+            });
+
+            // Add mouseover event listener to continue drawing while dragging
+            box.addEventListener('mouseover', function() {
+                if (isDrawing) {
+                    draw(box);
+                }
+            });
+
+            // Add mouseup event listener to stop drawing
+            box.addEventListener('mouseup', function() {
+                isDrawing = false;
+            });
+
+            function draw(element) {
+                element.style.backgroundColor = 'black';
+                isDrawing = true;
             }
-        });
 
-        // Add mouseup event listener to stop drawing
-        box.addEventListener('mouseup', function() {
-            isDrawing = false;
-        });
-
-        function draw(element) {
-            element.style.backgroundColor = 'black';
-            isDrawing = true;
         }
-
     }
+}
+
+function clearGrid(){
+    container.innerHTML = '';
 }
