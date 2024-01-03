d191370f7790592f482e01e87630b4e134454b61
AcidTCell <cimafrancajasper.personal@gmail.com>
2024-01-04 05:31:45 +0800

    Add random rgb generator and rainbow grid button

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 let container = document.querySelector('#container');
 let newDiv = document.createElement('div');
 let gridButton = document.querySelector('#gridButton');
+let gridRainbowButton = document.querySelector('#gridRainbowButton');
 let clearButton = document.querySelector('#clearButton')
 
 
 gridButton.addEventListener('click', getGridSize);
+gridRainbowButton.addEventListener('click', getRainbowGridSize);
 clearButton.addEventListener('click', clearGrid);
 
 
@@ -19,6 +21,20 @@ function getGridSize(){
     }
     else{
         createBlackGrid(column, row);
+    }
+}
+
+function getRainbowGridSize(){
+    clearGrid();
+    let gridSize = prompt('What dimensions are you seeking? ');
+    let column = gridSize;
+    let row = gridSize;
+    if (gridSize > 100){
+        alert('Value too high, please try again!');
+        getRainbowGridSize();
+    }
+    else{
+        createRainbowGrid(column, row);
     }
 }
 
@@ -47,6 +63,45 @@ function createBlackGrid(column, row){
 
             function draw(element) {
                 element.style.backgroundColor = 'black';
+                isDrawing = true;
+            }
+        }
+    }
+}
+
+function createRainbowGrid(column, row){
+    for (let i = 1; i<=row; i++){
+        let row = document.createElement('div');
+        row.classList.add('row');
+        container.appendChild(row);
+        for (let j = 1; j<=column; j++){
+            let box = document.createElement('div');
+            box.classList.add('box');
+            row.appendChild(box);
+            box.addEventListener('mousedown', function() {
+                drawRgb(box);
+            });
+
+            box.addEventListener('mouseover', function() {
+                if (isDrawing) {
+
+                    drawRgb(box);
+                }
+            });
+
+            box.addEventListener('mouseup', function() {
+                isDrawing = false;
+            });
+
+            function randomRgb() {
+                var o = Math.round, r = Math.random, s = 255;
+                isDrawing = true;
+                return 'rgba(' + o(r()*s) + ',' + o(r()*s) + ',' + o(r()*s) + ',' + r().toFixed(1) + ')';
+
+            }
+            function drawRgb(element) {
+                let color = randomRgb();
+                element.style.backgroundColor = color;
                 isDrawing = true;
             }
         }
